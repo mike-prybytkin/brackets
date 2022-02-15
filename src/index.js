04722b28fde366c9fce649522e40d11348bc186f
@Mike-Prybytkin <prybytkin@bk.ru>
2022-02-16 00:55:21 +0300

    feat: add solution v1.0

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(...expression) {
+  let string = expression.slice("").join("");
+  let newArr = [];
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === ",") {
+      continue;
+    } else {
+      newArr.push(string[i]);
+    }
+  }
+
+  const stack = [];
+  let counter = 0;
+
+  for (const symbol of newArr) {
+    if (
+      symbol === "(" ||
+      symbol === "{" ||
+      symbol === "[" ||
+      (symbol === "|" && counter % 2 === 0)
+    ) {
+      if (symbol === "|") {
+        counter++;
+      }
+      stack.push(symbol);
+    } else if (
+      symbol === ")" ||
+      symbol === "}" ||
+      symbol === "]" ||
+      (symbol === "|" && counter % 2 !== 0)
+    ) {
+      if (symbol === ")") {
+        if (stack.pop() !== "(") {
+          return false;
+        }
+      }
+      if (symbol === "}") {
+        if (stack.pop() !== "{") {
+          return false;
+        }
+      }
+      if (symbol === "]") {
+        if (stack.pop() !== "[") {
+          return false;
+        }
+      }
+      if (symbol === "|") {
+        counter--;
+        if (stack.pop() !== symbol) {
+          return false;
+        }
+      }
+    }
+  }
+  return stack.length === 0;
+};
