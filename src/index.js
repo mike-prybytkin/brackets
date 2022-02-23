521460491da190eef2dc58e0ae4509e38eefbcde
@Mike-Prybytkin <prybytkin@bk.ru>
2022-02-23 18:49:52 +0300

    fix: fix script

--- src/index.js
+++ src/index.js
@@ -1,57 +1,15 @@
-module.exports = function check(...expression) {
-  let string = expression.slice("").join("");
-  let newArr = [];
-
-  for (let i = 0; i < string.length; i++) {
-    if (string[i] === ",") {
-      continue;
-    } else {
-      newArr.push(string[i]);
-    }
-  }
-
-  const stack = [];
-  let counter = 0;
-
-  for (const symbol of newArr) {
-    if (
-      symbol === "(" ||
-      symbol === "{" ||
-      symbol === "[" ||
-      (symbol === "|" && counter % 2 === 0)
-    ) {
-      if (symbol === "|") {
-        counter++;
+module.exports = function check(str, bracketsConfig) {
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    bracketsConfig.forEach((el) => {
+      if (str[i] === el[1] && stack[stack.length - 1] === el[0]) {
+        stack.pop(str[i]);
+      } else if (str[i] === el[0]) {
+        stack.push(str[i]);
+      } else {
+        return false;
       }
-      stack.push(symbol);
-    } else if (
-      symbol === ")" ||
-      symbol === "}" ||
-      symbol === "]" ||
-      (symbol === "|" && counter % 2 !== 0)
-    ) {
-      if (symbol === ")") {
-        if (stack.pop() !== "(") {
-          return false;
-        }
-      }
-      if (symbol === "}") {
-        if (stack.pop() !== "{") {
-          return false;
-        }
-      }
-      if (symbol === "]") {
-        if (stack.pop() !== "[") {
-          return false;
-        }
-      }
-      if (symbol === "|") {
-        counter--;
-        if (stack.pop() !== symbol) {
-          return false;
-        }
-      }
-    }
+    });
   }
   return stack.length === 0;
 };
