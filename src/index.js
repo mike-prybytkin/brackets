ab4c3e5730fd5ad4e4a7f7c89d28b634af9513af
@Mike-Prybytkin <prybytkin@bk.ru>
2022-02-23 18:57:08 +0300

    fix: add str.length check

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 module.exports = function check(str, bracketsConfig) {
+  if (str.length % 2 !== 0) {
+    return false;
+  }
   let stack = [];
   for (let i = 0; i < str.length; i++) {
     bracketsConfig.forEach((el) => {
